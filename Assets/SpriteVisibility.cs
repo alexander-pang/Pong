c788fc1754d8dc07c5c900bed4599fcd2b698141
agrygo99 <agrygo@uiowa.edu>
2020-09-15 18:54:33 -0500

    Updated ball and paddles, added in Pong Title and spriteVisibility script

---
+++ Assets/SpriteVisibility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteVisibility : MonoBehaviour
+{
+    SpriteRenderer render;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = this.gameObject.GetComponent<SpriteRenderer> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //got this from a tutorial, maybe change press Q to be a start game button
+        if (Input.GetKeyDown(KeyCode.Q)){
+            render.enabled= !render.enabled;
+        }
+    }
+}
